1e0727330a7a0649b779ffdceb72c9c56ca47233
grouls <roulstone.gary@gmail.com>
2023-12-21 15:19:48 +0000

    Add todo list and improve current code

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,66 @@ import './style.css';
 
 const API_URL = 'https://restcountries.com/v3.1/name';
 
+const noResults = {
+  name: {
+    common: 'No Results found.'
+  }
+}
+const loading = {
+  name: {
+    common: 'Loading...'
+  }
+}
+/*
+  - Add validation, string only
+  - Clear list whenever the user clears the input
+  - useReducer instead of useState
+  - make a better loading/error/empty state
+  - improve API status handling
+  - Split file into Component and API folder
+  - improve styles
+ */
 export const App: FC = () => {
   const [country, setCountry] = useState('');
+  const [selection, setSelection] = useState('');
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       if (country) {
+        setData([loading]);
         const response = await fetch(`${API_URL}/${country}`);
         const json = await response.json();
-        setData(json);
+        json.status !== 404 ? setData(json) : setData([noResults]);
       }
     }
     fetchData();
-
     return () => {};
   }, [country]);
 
-  const handleChange = (e) => setCountry(e.target.value);
-
+  const handleChange = (e) => {
+    const input = e.target.value;
+    if(input === '') setData([])
+    setCountry(input);
+  };
+  const handleClick = (e) => {
+    const input = e.target.name;
+    setSelection(input);
+    setData([])
+    setCountry('')
+  };
   return (
     <div className="container">
+      <h1>{selection}</h1>
       <input
         type="input"
         placeholder="Enter a country"
         value={country}
         onChange={(e) => handleChange(e)}
       />
-      {/* <ul>
-        <li>
-          <button type="submit">Option 1</button>
-        </li>
-        <li>
-          <button type="submit">Option 2</button>
-        </li>
-      </ul> */}
+      {data.length !== 0 && <ul>
+        {data.map(d => <li><button name={d.name.common} type='submit' onClick={(e) => handleClick(e)}>{d.name.common}</button></li>)}
+      </ul>}
     </div>
   );
 };
